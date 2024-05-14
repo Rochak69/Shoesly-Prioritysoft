ec9c8a47bb9be4b1660a6079ffa45bbf63cc8be2
Rochak-ES <rochak@esignature.com.np>
2024-05-14 17:16:09 +0545

    minor fixes

--- lib/constants/app_images.dart
+++ lib/constants/app_images.dart
@@ -1,6 +1,7 @@
 class AppImages {
   static const cartSvg = 'assets/images/cart.svg';
   static const addedToCart = 'assets/images/tick-circle.png';
+  static const trashSvg = 'assets/images/trash.svg';
   static const brandPng = 'assets/images/brand.png';
   static const shoePng = 'assets/images/shoe.png';
 }
--- lib/features/cart/presentation/screen/cart_screen.dart
+++ lib/features/cart/presentation/screen/cart_screen.dart
@@ -1,9 +1,14 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:project_init/constants/app_colors.dart';
+import 'package:project_init/constants/app_images.dart';
 import 'package:project_init/features/common/app_scaffold.dart';
 import 'package:project_init/features/common/app_spacing.dart';
+import 'package:project_init/features/common/model/product_model.dart';
+import 'package:project_init/features/common/shoe_container.dart';
 
 @RoutePage()
 class CartScreen extends StatelessWidget {
@@ -24,27 +29,42 @@ class CartScreen extends StatelessWidget {
         itemCount: 10,
         separatorBuilder: (context, index) => const VerticalSpacing(30),
         itemBuilder: (context, index) => Slidable(
-          endActionPane: ActionPane(
-            motion: const ScrollMotion(),
-            children: [
-              Container(
-                width: 80,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(20),
-                    topLeft: Radius.circular(20),
+            endActionPane: ActionPane(
+              extentRatio: 0.2,
+              motion: const ScrollMotion(),
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    width: 80,
+                    height: double.infinity,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        topLeft: Radius.circular(20),
+                      ),
+                      color: AppColors.colorRed,
+                    ),
+                    child: Align(
+                      child: SvgPicture.asset(
+                        AppImages.trashSvg,
+                        width: 24,
+                        height: 24,
+                      ),
+                    ),
                   ),
-                  color: AppColors.colorRed,
                 ),
-                child: const Icon(Icons.delete),
-              ),
-            ],
-          ),
+              ],
+            ),
 
-          // The child of the Slidable is what the user sees when the
-          // component is not dragged.
-          child: const ListTile(title: Text('Slide me')),
-        ),
+            // The child of the Slidable is what the user sees when the
+            // component is not dragged.
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Row(
+                children: [],
+              ),
+            )),
       ),
     );
   }
