e8e4ce07b5adea6a4419a1a2ffe635579bc7dff6
Rochak69 <rochakshrestha1999@gmail.com>
2024-05-19 23:07:21 +0545

    remove comments

--- lib/core/preferences/preferences.dart
+++ lib/core/preferences/preferences.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:injectable/injectable.dart';
 
@@ -33,10 +32,10 @@ class Preference {
 
 @injectable
 class Preferences {
-  final FlutterSecureStorage _secureStorage;
   const Preferences(
     this._secureStorage,
   );
+  final FlutterSecureStorage _secureStorage;
 
   Future<void> saveString(String name, String value) async {
     await _secureStorage.write(key: name.toLowerCase(), value: value);
